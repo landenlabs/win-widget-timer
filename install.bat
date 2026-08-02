575f7118afaec24052d7ce8b0e7c1dc920e72c8e
unknown <landenlabs@gmail.com>
2026-08-01 23:05:41 -0400

    installer to c drive

---
+++ install.bat
@@ -0,0 +1,17 @@
+@echo off
+
+echo Build and install WinWidgetTimer
+
+@rem TODO - get bin directory from a global environment. 
+set dstDir=c:\opt\bin
+
+
+:: Kill widget if running, so it can be re-built
+taskkill /IM WinWidgetTimer.exe /F 2>nul
+dotnet publish WinWidgetTimer.csproj -c Release --self-contained false
+
+:: Make directory to hold the dll's and assets
+mkdir %dstDir%\winwidgets 2>nul
+xcopy /E /Y bin\Release\net10.0-windows\publish\* %dstDir%\winwidgets\
+
+echo start "" %dstDir%\winwidgets\WinWidgetTimer.exe > %dstDir%\WinWidgetTimer.bat
